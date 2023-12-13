57768c4bf0f9c1e024c2dfe6410ce890b0de13d5
luis <luishm2024@gmail.com>
2023-12-13 14:42:44 -0500

    creating new project

---
+++ src/main/java/com/sunset/rider/msvclabroom/MsvcLabRoomApplication.java
@@ -0,0 +1,13 @@
+package com.sunset.rider.msvclabroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcLabRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcLabRoomApplication.class, args);
+	}
+
+}
